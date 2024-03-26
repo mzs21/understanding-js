21481a32da004254f8c68de5d114f0e5e1b8f7de
Md Mashroor Zilan Snigdho <snigdho2111@gmail.com>
2024-03-26 04:38:01 +0300

    update: comment

--- prototype.js
+++ prototype.js
@@ -8,183 +8,196 @@ Prototype: It’s a blue print of an object
 
 
 // EX 1
+/*
+const personMethods = {
+    eat() {
+        console.log('Person is eating')
+    },
+    sleep() {
+        console.log('Person is sleeping')
+    },
+    play() {
+        console.log('Person is playing');
+    }
+};
 
-// const personMethods = {
-//     eat() {
-//         console.log('Person is eating')
-//     },
-//     sleep() {
-//         console.log('Person is sleeping')
-//     },
-//     play() {
-//         console.log('Person is playing');
-//     }
-// };
+function Person(name, age) {
+    let person = Object.create(personMethods)
 
-// function Person(name, age) {
-//     let person = Object.create(personMethods)
+    person.name = name;
+    person.age = age
 
-//     person.name = name;
-//     person.age = age
-
-//     return person
-// }
+    return person
+}
 
 
-// const X = Person('X', 13)
+const X = Person('X', 13)
 
-// const Y = Person('Y', 15)
+const Y = Person('Y', 15)
 
-// X.eat();
-// Y.play()
-
+X.eat();
+Y.play()
+*/
 
 // EX 2
 
-// function test() {}
+/*
+function test() {}
 
-// console.dir(test)
-
+console.dir(test)
+*/
 
 // EX 3
+/*
+function Person(name, age) {
+    let person = Object.create(Person.prototype)
 
-// function Person(name, age) {
-//     let person = Object.create(Person.prototype)
+    person.name = name;
+    person.age = age
 
-//     person.name = name;
-//     person.age = age
-
-//     return person
-// }
+    return person
+}
 
 
-// Person.prototype = {
-//     eat() {
-//         console.log('Person is eating')
-//     },
-//     sleep() {
-//         console.log('Person is sleeping')
-//     },
-//     play() {
-//         console.log('Person is playing');
-//     }
-// }
+Person.prototype = {
+    eat() {
+        console.log('Person is eating')
+    },
+    sleep() {
+        console.log('Person is sleeping')
+    },
+    play() {
+        console.log('Person is playing');
+    }
+}
 
-// const X = Person('X', 13)
+const X = Person('X', 13)
 
-// const Y = Person('Y', 15)
+const Y = Person('Y', 15)
 
-// X.eat();
-// Y.play()
+X.eat();
+Y.play()
+*/
 
 // EX 4
 
-// function Person(name, age) {
-//     let person = Object.create(Person.prototype)
+/*
+function Person(name, age) {
+    let person = Object.create(Person.prototype)
 
-//     person.name = name;
-//     person.age = age
+    person.name = name;
+    person.age = age
 
-//     return person
-// }
+    return person
+}
 
-// function PersonWithNew(name, age) {
-//     // let person = Object.create(PersonWithNew.prototype)
-//     // becomes 
-//     // let this = Object.create(PersonWithNew.prototype)
+function PersonWithNew(name, age) {
+    // let person = Object.create(PersonWithNew.prototype)
+    // becomes 
+    // let this = Object.create(PersonWithNew.prototype)
 
-//     // person.name = name;
-//     // becomes
-//     this.name = name;
+    // person.name = name;
+    // becomes
+    this.name = name;
 
-//     // person.age = age
-//     // becomes
-//     this.age = age;
+    // person.age = age
+    // becomes
+    this.age = age;
 
-//     // return person
-//     // becomes
-//     // return this
-// }
-
-
-// Person.prototype = {
-//     eat() {
-//         console.log('Person is eating')
-//     },
-//     sleep() {
-//         console.log('Person is sleeping')
-//     },
-//     play() {
-//         console.log('Person is playing');
-//     }
-// }
-
-// const X = Person('X', 13)
-
-// const Y = Person('Y', 15)
+    // return person
+    // becomes
+    // return this
+}
 
 
-// const X = new PersonWithNew('X', 13)
+Person.prototype = {
+    eat() {
+        console.log('Person is eating')
+    },
+    sleep() {
+        console.log('Person is sleeping')
+    },
+    play() {
+        console.log('Person is playing');
+    }
+}
 
-// const Y = new PersonWithNew('Y', 15)
+const X = Person('X', 13)
+
+const Y = Person('Y', 15)
+
+
+const X = new PersonWithNew('X', 13)
+
+const Y = new PersonWithNew('Y', 15)
+*/
+
 
 // EX 5
 
 // Constructed version of EX 4
 
-// function Person(name, age) {
+/*
+function Person(name, age) {
 
-//     this.name = name;
-//     this.age = age;
-// }
+    this.name = name;
+    this.age = age;
+}
 
-// Person.prototype = {
-//     eat() {
-//         console.log('Person is eating')
-//     },
-//     sleep() {
-//         console.log('Person is sleeping')
-//     },
-//     play() {
-//         console.log('Person is playing');
-//     }
-// }
+Person.prototype = {
+    eat() {
+        console.log('Person is eating')
+    },
+    sleep() {
+        console.log('Person is sleeping')
+    },
+    play() {
+        console.log('Person is playing');
+    }
+}
 
-// const X = new Person('X', 13)
+const X = new Person('X', 13)
 
-// const Y = new Person('Y', 15)
+const Y = new Person('Y', 15)
+*/
+
 
 // EX 6
 
 // EX 5 as a Class
 
-// class Person {
-//     constructor(name, age) {
-//         this.name = name;
-//         this.age = age;
-//     }
+/*
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
 
-//     eat() {
-//         console.log('Person is eating')
-//     }
-//     sleep() {
-//         console.log('Person is sleeping')
-//     }
-//     play() {
-//         console.log('Person is playing');
-//     }
-// }
+    eat() {
+        console.log('Person is eating')
+    }
+    sleep() {
+        console.log('Person is sleeping')
+    }
+    play() {
+        console.log('Person is playing');
+    }
+}
 
-// const X = new Person('X', 13)
+const X = new Person('X', 13)
 
-// const Y = new Person('Y', 15)
+const Y = new Person('Y', 15)
 
-// Y.eat()
+Y.eat()
+*/
+
 
 // EX 7
 
+/*
 let array = []
 
 let newArray = new Array()
 
 console.dir(Array.prototype)
+*/
