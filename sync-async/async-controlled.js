2ed773005756ffe21fafbf182ea6e365db8d7123
Md Mashroor Zilan Snigdho <snigdho2111@gmail.com>
2024-06-26 02:26:04 +0300

    :sparkles: add: fixed asynchronous code explanation

---
+++ sync-async/async-controlled.js
@@ -0,0 +1,23 @@
+const takeOrder = (customer, callback) => {
+    console.log(`Take order from ${customer}`);
+    callback(customer)
+}
+
+const processOrder = (customer, callback) => {
+    console.log(`Processing order for ${customer}`)
+
+    setTimeout(() => {
+        console.log(`Cooking completed`);
+        console.log(`Order processed for ${customer}`)
+        callback(customer)
+    }, 3000);
+
+
+}
+
+const completeOrder = (customer) => {
+    console.log(`Completed order for ${customer}`);
+}
+
+takeOrder('Customer 1', (customer) => processOrder(customer, (customer) => completeOrder(customer)))
+
