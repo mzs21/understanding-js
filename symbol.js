33c7687beb86b64a5232a35da92c41e118519a04
Md Mashroor Zilan Snigdho <snigdho2111@gmail.com>
2024-07-10 15:54:03 +0300

    :memo: update: symbol examples

--- symbol.js
+++ symbol.js
@@ -54,8 +54,59 @@ console.log(symbol1 === symbol2);  // true
 
 // Symbol can be used an object's property
 
+/*
 let sportName = Symbol('This is a sport')
 
 let sport = { name: 'X', [sportName]: 'XYZ' }
 
 console.log(sport);
+*/
+
+// * MARK: EX 6
+
+/*
+let object = {}
+
+object.name = 'X'
+object.year = 1234
+
+object[Symbol('rank')] = 1
+
+console.log(object);
+
+// But the Symbol can't be accessed using for loop.
+
+for (const key in object) {
+    let value = object[key]
+    console.log(key, value);
+}
+
+// To access the symbol property, we need to use 'getOwnPropertySymbols'
+
+let symbols = Object.getOwnPropertySymbols(object)
+
+for (const symbol of symbols) {
+    console.log(symbol, object[symbol])
+}
+
+// Symbol is used as an unique identifier.
+// JS can ignore the Symbol in normal loops.
+*/
+
+// * MARK: EX 7
+
+let arr = [1, 2, 3]
+
+console.log(arr.includes(2));
+console.log(arr['includes'](2));
+
+
+// We can make our own method and add it to a Master Object without polluting globally.
+
+let includes = Symbol('My inlcude method')
+
+Array.prototype[includes] = () => console.log('My own Include Method');
+
+arr[includes](2)
+
+// Since, Symbol is an unique identifier, we can make a lot of versions of 'includes' without affecting the original 'includes' method of Array.
